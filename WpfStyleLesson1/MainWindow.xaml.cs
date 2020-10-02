72c48cf1fc7c0cafcc3e43378b1a325bf3978c7b
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-02 14:19:33 +0300

    Add project files.

---
+++ WpfStyleLesson1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfStyleLesson1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            //Application.Current.Resources["secondStyle"]
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Resources["firstStyle"] = Application.Current.Resources["thirdStyle"];
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Resources["lightPinkColor"] = new SolidColorBrush(Colors.Yellow);
+        }
+    }
+}
